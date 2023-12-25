709eaa7a767519fa3177c5547a3f20ab16306823
aakarpovaa <145562322+aakarpovaa@users.noreply.github.com>
2023-12-25 14:02:18 +0300

    Add files)

---
+++ Задание2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+#include <set>
+
+bool containsNearbyAlmostDuplicate(std::vector<int>& nums, int indexDiff, int valueDiff) {
+	if (nums.size() <= 1 || indexDiff <= 0 || valueDiff < 0) {
+		return false;
+	}
+
+	std::set<long long> window;
+
+	for (int i = 0; i < nums.size(); ++i) {
+		if (i > indexDiff) {
+			window.erase(nums[i - indexDiff - 1]);
+		}
+
+		auto it = window.lower_bound(static_cast<long long>(nums[i]) - valueDiff);
+		if (it != window.end() && *it <= static_cast<long long>(nums[i]) + valueDiff) {
+			return true;
+		}
+
+		window.insert(nums[i]);
+	}
+
+	return false;
+}
+
+int main() {
+
+	std::vector<int> nums = { 4, 3, 7, 2, 6, 8 };
+	int indexDiff = 2;
+	int valueDiff = 3;
+	std::cout << (containsNearbyAlmostDuplicate(nums, indexDiff, valueDiff) ? "true" : "false") << std::endl;
+
+	return 0;
+}
---
+++ Задание3.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <queue>
+
+class MedianFinder {
+private:
+	std::priority_queue<int> maxHeap; 
+	std::priority_queue<int, std::vector<int>, std::greater<int>> minHeap;  
+
+public:
+	MedianFinder() {}  // класс MedianFinder	
+
+	void addNum(int num) {
+		if (maxHeap.empty() || num <= maxHeap.top()) {
+			maxHeap.push(num);
+		}
+		else {
+			minHeap.push(num);
+		}
+
+		if (maxHeap.size() > minHeap.size() + 1) {
+			minHeap.push(maxHeap.top());
+			maxHeap.pop();
+		}
+		else if (minHeap.size() > maxHeap.size()) {
+			maxHeap.push(minHeap.top());
+			minHeap.pop();
+		}
+	}
+
+	double findMedian() {
+		if (maxHeap.size() == minHeap.size()) {
+			return (maxHeap.top() + minHeap.top()) / 2.0;
+		}
+		else {
+			return maxHeap.top();
+		}
+	}
+};
+
+int main() {
+	MedianFinder medianFinder;
+	medianFinder.addNum(2);
+	medianFinder.addNum(10);
+	double result1 = medianFinder.findMedian(); // Возращает 6
+	std::cout << result1 << std::endl;
+
+	medianFinder.addNum(15);
+	double result2 = medianFinder.findMedian(); // Возвращает 10
+	std::cout << result2 << std::endl;
+
+	return 0;
+}
