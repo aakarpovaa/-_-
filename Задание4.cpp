0c29f36e8aa7c454b29800ee6eba87df70694b9d
aakarpovaa <145562322+aakarpovaa@users.noreply.github.com>
2023-12-27 13:26:02 +0300

    Add files)

---
+++ Задание4.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class NQueens {
+public:
+	vector<vector<string>> solveNQueens(int n) {
+		vector<vector<string>> result;
+		vector<string> board(n, string(n, '.'));
+
+		solveNQueensHelper(board, 0, result);
+
+		return result;
+	}
+
+private:
+	void solveNQueensHelper(vector<string>& board, int row, vector<vector<string>>& result) {
+		if (row == board.size()) {
+			result.push_back(board);
+			return;
+		}
+
+		for (int col = 0; col < board.size(); ++col) {
+			if (isSafe(board, row, col)) {
+				board[row][col] = 'Q';
+				solveNQueensHelper(board, row + 1, result);
+				board[row][col] = '.';  
+			}
+		}
+	}
+
+	bool isSafe(const vector<string>& board, int row, int col) {
+		
+		for (int i = 0; i < row; ++i) {
+			if (board[i][col] == 'Q') {
+				return false;
+			}
+		}
+
+		for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; --i, --j) {
+			if (board[i][j] == 'Q') {
+				return false;
+			}
+		}
+
+		for (int i = row - 1, j = col + 1; i >= 0 && j < board.size(); --i, ++j) {
+			if (board[i][j] == 'Q') {
+				return false;
+			}
+		}
+
+		return true;
+	}
+};
+
+int main() {
+	int n = 5; // тут указываем любое нужное нам n и живём счастливо
+	NQueens solver;
+	vector<vector<string>> result = solver.solveNQueens(n);
+
+	for (const auto& solution : result) {
+		for (const auto& row : solution) {
+			cout << row << endl;
+		}
+		cout << "---------" << endl;
+	}
+
+
+	return 0;
+}
+// С Наступающим Новым годом!!!
---
+++ задание5.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <vector>
+#include <stack>
+#include <algorithm>
+
+using namespace std;
+
+int maximalRectangle(vector<vector<char>>& matrix) {
+	if (matrix.empty() || matrix[0].empty()) {
+		return 0;
+	}
+
+	int rows = matrix.size();
+	int cols = matrix[0].size();
+	vector<vector<int>> heights(rows, vector<int>(cols, 0));
+	int maxArea = 0;
+
+	// Преобразование символов в целые числа и создание массива высот
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+			matrix[i][j] -= '0';
+			if (i > 0 && matrix[i][j] == 1) {
+				heights[i][j] = heights[i - 1][j] + 1;
+			}
+			else {
+				heights[i][j] = matrix[i][j];
+			}
+		}
+	}
+
+	for (int i = 0; i < rows; ++i) {
+		stack<int> s;
+		for (int j = 0; j <= cols; ++j) {
+			while (!s.empty() && (j == cols || heights[i][j] < heights[i][s.top()])) {
+				int height = heights[i][s.top()];
+				s.pop();
+				int width = s.empty() ? j : j - s.top() - 1;
+				maxArea = max(maxArea, height * width);
+			}
+			s.push(j);
+		}
+	}
+
+	return maxArea;
+}
+
+int main() {
+	vector<vector<char>> matrix1 = {
+		{'1', '0', '1', '0', '0'},
+		{'1', '0', '1', '1', '1'},
+		{'1', '1', '1', '1', '1'},
+		{'1', '0', '0', '1', '0'}
+	};
+
+	cout << maximalRectangle(matrix1) << endl; // Вывод: 6
+
+	return 0;
+}
